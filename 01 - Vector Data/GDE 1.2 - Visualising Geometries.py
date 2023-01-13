58a13e791743adf7ec00e90ee1da3fdce08c4915
timdikland@gmail.com <timdikland@gmail.com>
2023-01-13 14:18:41 +0000

    Finish V1 of module 1

--- 01 - Vector Data/GDE 1.2 - Visualising Geometries.py
+++ 01 - Vector Data/GDE 1.2 - Visualising Geometries.py
@@ -1,2 +1,59 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Visualising Geometries
+# MAGIC 
+# MAGIC Often geometries are fairly complex, and it is hard or even impossible to obtain useful information from a textual representation. This is where visualisation of geometries helps to quickly understand the shape or position of some geometry.
+# MAGIC 
+# MAGIC ## Learning objectives
+# MAGIC By the end of this notebook you should be able to:
+# MAGIC - Understand how the `%%mosaic_kepler` magic command works
+# MAGIC - Visualise WKT/WKB or Mosaic internel geometries
+# MAGIC - Navigate the Kepler visualisation
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Setup
+# MAGIC Run the setup script. This will import some needed modules and install the mosaic library. The mosaic library and its installation will be explained in depth in module (TODO)
+
+# COMMAND ----------
+
+# MAGIC %run ../Includes/Setup-1
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Geometry visualisation with kepler.gl
+# MAGIC Mosaic registers a magic function in the notebook to easily produce visualisations using [kepler.gl](https://kepler.gl/). 
+# MAGIC 
+# MAGIC For a visualisation we need 4 parameters:
+# MAGIC - _dataframe_: the dataframe that contains the column with geometries that need to be visualised
+# MAGIC - _column-name_: the name of the column in the aforementioned dataframe, which contains the geometries. The geometries must be in `WKT`, `WKB` or `Mosiac Internal` format
+# MAGIC - _feature-type_: the type of spatial feature to be visualised. In this case this is always equal to the string `geometry`
+# MAGIC - _limit_: the amount of geometries to plot. This is an optional parameter. When it is not provided it plots up to 1000 geometries by default
+# MAGIC 
+# MAGIC The kepler magic command can be used as follows:
+# MAGIC 
+# MAGIC ```
+# MAGIC %%mosaic_kepler
+# MAGIC dataframe column-name feature-type [<limit>]
+# MAGIC ```
+# MAGIC 
+# MAGIC Let's see that in action!
+
+# COMMAND ----------
+
+# Create some geometries to visualise
+databricks_hq_geojson = '{"type": "Polygon", "coordinates": [[[-122.39343385977625, 37.79132004377702], [-122.39370019061133, 37.791537245784454], [-122.39400700373288, 37.79128973646942], [-122.39373641160483, 37.79107926871191], [-122.39343385977625, 37.79132004377702]]]}'
+san_fransisco_road = '{"coordinates": [[-122.39492103462949, 37.79181899994789], [-122.39340774317989, 37.790613052399536], [-122.39251569769378, 37.791302887017565], [-122.3940767772944, 37.79254658717453]], "type": "LineString"}'
+bus_stop = '{"coordinates": [-122.39315228821998, 37.791682551863005], "type": "Point"}'
+
+df = spark.createDataFrame([(databricks_hq_geojson,), (san_fransisco_road,), (bus_stop,)], "geom_geojson STRING")
+
+# Note that the %%mosaic_kepler command cannot visualise geojson! We have to convert it into Mosaic's internal format
+df_viz = df.withColumn("geom", mos.st_geomfromgeojson(F.col("geom_geojson")))
+
+# COMMAND ----------
+
+# MAGIC %%mosaic_kepler
+# MAGIC df_viz "geom" "geometry" 3
--- 01 - Vector Data/GDE 1.3L - Getting Started with Vector Geometries.py
+++ 01 - Vector Data/GDE 1.3L - Getting Started with Vector Geometries.py
@@ -1,2 +1,82 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Getting Started with Vector Geometries
+# MAGIC 
+# MAGIC this notebook provides a hands-on review on the basic construction, conversion and visualisation of geospatial vector data.
+# MAGIC 
+# MAGIC ## Learning Objectives
+# MAGIC By the end of this lab you should be able to
+# MAGIC - Express a spatial feature on Earth in a vector format
+# MAGIC - Read vector data into Mosaic's internal format
+# MAGIC - Convert between common vector formats
+# MAGIC - Visualise geometries using kepler.gl 
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Setup
+# MAGIC Run the following script to setup the lab environment.
+
+# COMMAND ----------
+
+# MAGIC %run ../Includes/Setup-1
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Represent your company HQ in WKT
+# MAGIC 
+# MAGIC In this notebook, we're going to work with the geometries of your company HQ. 
+# MAGIC 
+# MAGIC Use for instance [geojson.io](https://geojson.io) to find your company HQ on the map and draw a polygon.
+
+# COMMAND ----------
+
+# TODO
+company_hq_wkt = "..."
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Read the geometry using Mosaic.
+# MAGIC 
+# MAGIC Run the following cell to create a spark dataframe holding your geometry in WKT format.
+
+# COMMAND ----------
+
+df = spark.createDataFrame([("company_hq", company_hq_wkt)], "id STRING, geom_wkt STRING")
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Convert the WKT formatted geometry to Mosaic's internal format
+
+# COMMAND ----------
+
+# TODO
+df = df.withColumn("geom", ...)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Convert between formats
+# MAGIC 
+# MAGIC What is the binary representation of this geometry?
+
+# COMMAND ----------
+
+# TODO
+df = df.withColumn("geom_binary", ...)
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Visualise your HQ
+# MAGIC 
+# MAGIC Use the built-in kepler.gl support to visualise your company HQ.
+
+# COMMAND ----------
+
+# TODO
