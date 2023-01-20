24d54a9cf28d0d901ec5f0bad79baa5d4368cb90
timdikland@gmail.com <timdikland@gmail.com>
2023-01-20 14:13:47 +0000

    Build out lab exercise

--- 02 - Transforming Geometries/GDE 2.3L - Transforming Geometries.py
+++ 02 - Transforming Geometries/GDE 2.3L - Transforming Geometries.py
@@ -69,8 +69,51 @@ display(df_with_area)
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Can you calculate the area of three office locations, without calculating the area of these offices independently?
+# MAGIC Does the line between your companies HQ and Databricks HQ cross the equator?
 
 # COMMAND ----------
 
-# TODO
+# Setup geometries
+geom_between_hqs = "LINESTRING (<TODO>)"
+geom_equator = "LINESTRING (-180 0, 180 0)"
+
+schema = "geom_wkt STRING"
+df = spark.createDataFrame([{"geom1_wkt": geom_between_hqs, "geom2_wkt": geom_equator}], schema)
+df_geoms = (df
+    .withColumn("geom_hq", mos.st_geomfromwkt(F.col("geom1_wkt")))
+    .withColumn("geom_equator", mos.st_geomfromwkt(F.col("geom2_wkt")))
+    .select("geom_hq", "geom_equator")
+)
+
+# Find the correct predicate
+df_predicate = df_hq.withColumn("crosses_equator", "TODO")
+
+display(df_predicate)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **!!CHALLANGE!!** 
+# MAGIC 
+# MAGIC Two telecom providers have several radio towers in the San Fransisco area. These radio towers provide cell phone coverage within 5km of the tower. Given the location of these towers, which telecom provider provides the largest total area of coverage? 
+# MAGIC 
+# MAGIC HINT: 
+# MAGIC - use the ST_BUFFER function
+# MAGIC - use the `%%mosaic_kepler` magic to inspect your (intermediate) results
+
+# COMMAND ----------
+
+data = [
+    ("T-Mobile", "POINT (-122.426255 37.798106)"),
+    ("T-Mobile", "POINT (-122.428550 37.753449)"),
+    ("T-Mobile", "POINT (-122.501725 37.762229)"),
+    ("T-Mobile", "POINT (-122.501964 37.756874)"),
+    ("AT&T", "POINT (-122.410850 37.715457)"),
+    ("AT&T", "POINT (-122.499474 37.739015)"),
+    ("AT&T", "POINT (-122.414080 37.805109)")
+]
+
+df = spark.createDataFrame(data, "provider STRING, geom_wkt STRING")
+df_geoms = df.withColumn("point", mos.st_geomfromwkt(F.col("geom_wkt")))
+
+#TODO
