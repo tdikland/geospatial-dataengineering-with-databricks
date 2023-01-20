39eb4dc1250dde6759c247f390d90f74cfe036eb
timdikland@gmail.com <timdikland@gmail.com>
2023-01-20 14:53:14 +0000

    start with polygon join

--- 02 - Transforming Geometries/GDE 2.4 - Point-In-Polygon Joins.py
+++ 02 - Transforming Geometries/GDE 2.4 - Point-In-Polygon Joins.py
@@ -1,2 +1,21 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Point-In-Polygon Joins
+# MAGIC 
+# MAGIC The so called _point-in-polygon_ is a type of spatial join where a set of polygons is matched with all points which are contained in the respective polygons. In this notebook we'll motivate what problems are solved with this join, and how to execute these in Databricks
+# MAGIC 
+# MAGIC ## Learning Goals
+# MAGIC By the end of this notebook, you should be able to:
+# MAGIC - Formulate use cases that require a point-in-polygon join
+# MAGIC - Execute a geometric point-in-polygon query using Databricks and mosaic
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## What does the point-in-polygon join do?
+# MAGIC 
+# MAGIC The technical answer is not difficult
+
+# COMMAND ----------
+
 
