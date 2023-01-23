76b013af16385e9c2926246f0aefad26f24ec60f
timdikland@gmail.com <timdikland@gmail.com>
2023-01-23 10:48:40 +0000

    Finish lab exercise

--- 02 - Transforming Geometries/GDE 2.4 - Point-In-Polygon Joins.py
+++ 02 - Transforming Geometries/GDE 2.4 - Point-In-Polygon Joins.py
@@ -4,18 +4,94 @@
 # MAGIC 
 # MAGIC The so called _point-in-polygon_ is a type of spatial join where a set of polygons is matched with all points which are contained in the respective polygons. In this notebook we'll motivate what problems are solved with this join, and how to execute these in Databricks
 # MAGIC 
+# MAGIC ![point-in-polygon join](https://tdikland.github.io/geospatial-dataengineering-with-databricks/resources/module2/point-in-polygon.jpeg)
+# MAGIC _A graphical representation of a point-in-polygon problem_
+# MAGIC 
 # MAGIC ## Learning Goals
 # MAGIC By the end of this notebook, you should be able to:
 # MAGIC - Formulate use cases that require a point-in-polygon join
 # MAGIC - Execute a geometric point-in-polygon query using Databricks and mosaic
+# MAGIC - Understand the scaling behaviour of the "classical" point-in-polygon join
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Setup
+# MAGIC Run the setup script. This will import some needed modules and install the mosaic library. The mosaic library and its installation will be explained in depth in module 2.1
+
+# COMMAND ----------
+
+# MAGIC %run ../Includes/Setup-2
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## What does the point-in-polygon join do?
 # MAGIC 
-# MAGIC The technical answer is not difficult
+# MAGIC The technical answer is not difficult. A point-in-polygon (PIP) join, matches a dataset of points to a dataset of polygons using the _contains_ relation. The practical use cases are sometimes harder to recognize. Some examples are:
+# MAGIC 
+# MAGIC - Service coverage analysis
+# MAGIC - Retail site planning
+# MAGIC - Taxi fleet management
+# MAGIC 
+# MAGIC What do all of these use cases have on common? How would you leverage the PIP join in these use cases? What use cases within your company follow a similar pattern?
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Building a point-in-polygon query
+# MAGIC 
+# MAGIC Continuing using the toy example, let's see if we can find out which office are visited by Databricks founders. For this example it is assumed that we can track the cell phones of the founders.
 
+# COMMAND ----------
+
+# First of all we create a list of geometries that represent Databricks offices in California
+office_geoms = [
+    ("HQ", "POLYGON ((-122.39401298500093 37.79129663716428, -122.39401298500093 37.79129663716428, -122.39373988779624 37.79107781442336, -122.39343063138028 37.79132521190732, -122.39370182546828 37.79153952212819, -122.39401298500093 37.79129663716428))"),
+    ("Silicon Valley", "POLYGON ((-122.06188699366163 37.38742582735452, -122.06198446313843 37.38724441682429, -122.06179887057283 37.3868975077459, -122.06142501504554 37.386774444812204, -122.06133689195676 37.38694736939405, -122.06151046773734 37.387293217360735, -122.06188699366163 37.38742582735452))")
+]
+
+df_offices = (
+    spark
+    .createDataFrame(office_geoms, "location STRING, office_geom STRING")
+    .withColumn("geom", mos.st_geomfromwkt(F.col("office_geom")))
+)
+
+# Next we crate a list of points were the cell phones of the founders sent a ping from
+founder_pings = [
+    ("Ali", "POINT (-122.3940 37.7911)"),
+    ("Ali", "POINT (-122.3937 37.79131)"),
+    ("Ali", "POINT (-122.3950 37.7911)"),
+    ("Matei", "POINT (-122.3937 37.79131)"),
+    ("Matei", "POINT (-122.3940 37.7911)"),
+    ("Matei", "POINT (-122.0616 37.3870)"),
+    ("Reynold", "POINT (-122.3940 37.7911)"),
+    ("Reynold", "POINT (-122.3940 37.7911)"),
+    ("Reynold", "POINT (-122.3940 37.7911)"),
+]
+
+df_pings = (
+    spark
+    .createDataFrame(founder_pings, "founder STRING, ping STRING")
+    .withColumn("geom", mos.st_geomfromwkt(F.col("ping")))
+)
+
+# Execute the point-in-polygon join
+df_founders_in_offices = (
+    df_offices
+    .crossJoin(df_pings)
+    .where(mos.st_contains(df_offices.geom, df_pings.geom))
+    .select("founder", "location")
+)
+
+# Which founder prefers to work from home?
+display(df_founders_in_offices)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Complexity of the solution
+# MAGIC 
+# MAGIC In terms of code the query above does not look very complex at all! This section is about [time complexity](https://en.wikipedia.org/wiki/Time_complexity) of the PIP join, i.e. how does this computation scale as the number of polygons and points increase. 
+# MAGIC 
+# MAGIC In this case, if the number of polygons is denoted by `N` and the number of points by `M`, then the `ST_CONTAINS` operations is run `NxM` times. There are more nuances to this, but the bottomline is that this query _does not_ naturally scale to larger datasets. There are techniques to improve the scaling characteristic and they will be covered in the next modules. For now it is important to understand and remember that this query works fine on smaller datasets, but may fail to provide timely insights when the data size increases.
--- 02 - Transforming Geometries/GDE 2.5L - Spatial Analytics Lab.py
+++ 02 - Transforming Geometries/GDE 2.5L - Spatial Analytics Lab.py
@@ -1,2 +1,168 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Spatial Analytics Lab
+# MAGIC 
+# MAGIC This notebook provides a hands-on review on building spatial joins.
+# MAGIC 
+# MAGIC ## Learning Objectives
+# MAGIC By the end of this lab you should be able to
+# MAGIC - Execute a spatial join using the "classical" geometric approach
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Setup
+# MAGIC Run the setup script. This will import some needed modules and install the mosaic library. The mosaic library and its installation will be explained in depth in module 2.1
+
+# COMMAND ----------
+
+# MAGIC %run ../Includes/Setup-2
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Tracking Tractors
+# MAGIC 
+# MAGIC Suppose a fleet of tractors is fitted with a GPS tracker that sends its location every hour. Let's see if we can figure out what fields a tractor went to!
+
+# COMMAND ----------
+
+# Tractor positions measured using the GPS tracker.
+# The tuples represent the tractor_id, measurement time, and latitiude/longitude respectively.
+tractor_positions = [
+    ("2", "2023-01-14 02:00:00", -92.9559, 42.3954),
+    ("2", "2023-01-14 01:00:00", -92.9508, 42.4045),
+    ("3", "2023-01-14 01:00:00", -92.9638, 42.3975),
+    ("1", "2023-01-14 01:00:00", -92.9481, 42.3873),
+    ("3", "2023-01-14 02:00:00", -92.9504, 42.385),
+    ("1", "2023-01-14 02:00:00", -92.9508, 42.3883),
+    ("1", "2023-01-14 00:00:00", -92.9501, 42.4038),
+    ("1", "2023-01-14 00:00:00", -92.9601, 42.3872),
+    ("2", "2023-01-14 02:00:00", -92.9526, 42.4036),
+    ("3", "2023-01-14 01:00:00", -92.9544, 42.401),
+    ("3", "2023-01-14 00:00:00", -92.9577, 42.3884),
+    ("2", "2023-01-14 02:00:00", -92.9501, 42.3919),
+    ("3", "2023-01-14 01:00:00", -92.9483, 42.3963),
+    ("3", "2023-01-14 00:00:00", -92.9466, 42.3848),
+    ("2", "2023-01-14 00:00:00", -92.9544, 42.3897),
+    ("3", "2023-01-14 02:00:00", -92.9536, 42.3916),
+    ("2", "2023-01-14 01:00:00", -92.9518, 42.3995),
+    ("1", "2023-01-14 02:00:00", -92.9566, 42.3876),
+    ("3", "2023-01-14 02:00:00", -92.9617, 42.3899),
+    ("1", "2023-01-14 01:00:00", -92.9502, 42.3951),
+    ("1", "2023-01-14 01:00:00", -92.9622, 42.3891),
+    ("1", "2023-01-14 00:00:00", -92.9478, 42.3929),
+    ("2", "2023-01-14 00:00:00", -92.9572, 42.3873),
+    ("3", "2023-01-14 00:00:00", -92.951, 42.3928),
+    ("1", "2023-01-14 02:00:00", -92.945, 42.3998),
+    ("2", "2023-01-14 00:00:00", -92.9518, 42.3972),
+    ("2", "2023-01-14 01:00:00", -92.9588, 42.3862),
+]
+
+# The fields on which the tractors work.
+# The tuples represent the field_id and the field geometry in geojson format.
+fields = [
+    (
+        "A",
+        '{"coordinates": [[[-92.96383588484753, 42.40504553009913], [-92.96383588484753, 42.397895401557605], [-92.95423183265459, 42.397895401557605], [-92.95423183265459, 42.40504553009913], [-92.96383588484753, 42.40504553009913]]], "type": "Polygon"}',
+    ),
+    (
+        "B",
+        '{"coordinates": [[[-92.95415311091494, 42.40504553009913], [-92.95415311091494, 42.40155776430916], [-92.94462778046164, 42.40155776430916], [-92.94462778046164, 42.40504553009913], [-92.95415311091494, 42.40504553009913]]], "type": "Polygon"}',
+    ),
+    (
+        "C",
+        '{"coordinates": [[[-92.9541601413529, 42.40143038236528], [-92.9541601413529, 42.394308910728085], [-92.9446741717688, 42.394308910728085], [-92.9446741717688, 42.40143038236528], [-92.9541601413529, 42.40143038236528]]], "type": "Polygon"}',
+    ),
+    (
+        "D",
+        '{"coordinates": [[[-92.9541601413529, 42.39422170402594], [-92.9541601413529, 42.38361065101748], [-92.9446741717688, 42.38361065101748], [-92.9446741717688, 42.39422170402594], [-92.9541601413529, 42.39422170402594]]], "type": "Polygon"}',
+    ),
+    (
+        "E",
+        '{"coordinates": [[[-92.96400035876349, 42.397855213953534], [-92.96400035876349, 42.39076240710219], [-92.95888344570996, 42.39076240710219], [-92.95888344570996, 42.397855213953534], [-92.96400035876349, 42.397855213953534]]], "type": "Polygon"}',
+    ),
+    (
+        "F",
+        '{"coordinates": [[[-92.95880472397073, 42.39779707945081], [-92.95880472397073, 42.3906751954722], [-92.95435694570094, 42.3906751954722], [-92.95435694570094, 42.39779707945081], [-92.95880472397073, 42.39779707945081]]], "type": "Polygon"}',
+    ),
+    (
+        "G",
+        '{"coordinates": [[[-92.96396099789388, 42.39052984248633], [-92.96396099789388, 42.38361065101748], [-92.95427822396172, 42.38361065101748], [-92.95427822396172, 42.39052984248633], [-92.96396099789388, 42.39052984248633]]], "type": "Polygon"}',
+    ),
+]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Start off by creating dataframes with geometries specified above
+
+# COMMAND ----------
+
+fields_schema = "field_id STRING, geom_geojson STRING"
+df_fields_raw = spark.createDataFrame(fields, fields_schema)
+
+tractors_schema = "tractor_id STRING, timestamp STRING, latitude DOUBLE, longitude DOUBLE"
+df_tractors_raw = spark.createDataFrame(tractor_positions, tractors_schema)
+
+# Extract geometry. 
+df_fields = ... #TODO
+df_tractors = ... #TODO
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC visualise the geometries using the mosaic_kepler magic command
+
+# COMMAND ----------
+
+# MAGIC %%mosaic_kepler # TODO
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC Which tractors visited each fields? Structure your answer as follows:
+# MAGIC | field_id | tractor_ids |
+# MAGIC | --- | --- |
+# MAGIC | A | [1, 3] |
+# MAGIC | B | [] |
+# MAGIC | C | [2] |
+
+# COMMAND ----------
+
+# TODO
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Which fields where visited by each tractor? Structure your answer as follows:
+# MAGIC | tractor_id | field_ids |
+# MAGIC | --- | --- |
+# MAGIC | 1 | ["A", "F"] |
+# MAGIC | 2 | ["C"] |
+# MAGIC | 3 | ["G"] |
+
+# COMMAND ----------
+
+# TODO
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Find the amount of tractor location pings for each field.
+
+# COMMAND ----------
+
+# TODO
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **ADVANCED:** find the route of each tractor. (i.e. tractor 1: A -> F -> B -> G)
+# MAGIC 
+# MAGIC HINT: use a window function after the point in polygon calculation
+
+# COMMAND ----------
+
+# TODO
