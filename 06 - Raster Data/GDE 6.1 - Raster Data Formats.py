e90b5da913bc9b6f57d9f50e4ef148f3cc5f0301
timdikland@gmail.com <timdikland@gmail.com>
2023-01-25 09:57:28 +0000

    Raster format laypout

--- 06 - Raster Data/GDE 6.1 - Raster Data Formats.py
+++ 06 - Raster Data/GDE 6.1 - Raster Data Formats.py
@@ -17,3 +17,43 @@
 # MAGIC Raster data is a grid of values which are rendered on a map as pixels. Each of those pixels represent an area on the Earth's surface. The value of a pixel can represent continuous (e.g. elevation) or categorical (e.g. land use) data. This is in contrast with vector data which only represent specific features and their attributes on Earth's surface.
 # MAGIC 
 # MAGIC While this definition may sound a bit dry, you already have some experience working with rasters! The standard digital photo is a raster where each pixel represents the color value in red, green and blue (RGB). What makes a raster a _geospatial_ raster is the addition of spatial information that connects the image to a specific location on the Earth's surface. This spatial information includes the rasters extend, cell size, number of rows and columns and its coordinate reference system.
+# MAGIC 
+# MAGIC ![raster data format](https://tdikland.github.io/geospatial-dataengineering-with-databricks/resources/module6/raster_format.png)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ## Continuous rasters
+# MAGIC 
+# MAGIC TODO
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Categorical rasters
+# MAGIC 
+# MAGIC TODO
+# MAGIC 
+# MAGIC Note: binning of continuous raster -> categorical
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Resolution
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Multi-band raster data
+# MAGIC 
+# MAGIC (hyperspectal)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## GeoTIFF raster format
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## NetCDF raster format
